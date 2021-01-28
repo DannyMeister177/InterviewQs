1e649433f9f87b3946ce8ebb4cd7502dc72fcf76
DannyMeister177 <danny.goyer177@gmail.com>
2021-01-27 22:10:10 -0500

    Tried solution with StringBuffer but failed to account for int overflow

---
+++ src/reverse_integer/SolutionOldV1.java
@@ -0,0 +1,30 @@
+package reverse_integer;
+
+class SolutionOldV1 {
+    public static int reverse(int x) {
+        StringBuilder xStrBuilder = new StringBuilder(Integer.toString(x));
+        boolean isNegative = x<0;
+        if(isNegative) xStrBuilder.deleteCharAt(0);
+
+        // iterate through string representation of x in reverse order and
+        // append to xStrReverse
+        int length = xStrBuilder.length();
+        for(int i=0; i<length-1; i++){
+            xStrBuilder.insert(i, xStrBuilder.charAt(length-1));
+            xStrBuilder.deleteCharAt(length);
+        }
+
+        if(!isNegative) {
+            return Integer.parseInt(xStrBuilder.toString());
+        }
+        else{
+            xStrBuilder.insert(0,"-");
+            return Integer.parseInt(xStrBuilder.toString());
+        }
+    }
+
+    public static void main(String[] args){
+        // System.out.println(Integer.parseInt(args[0]));
+        System.out.println(SolutionOldV1.reverse(Integer.parseInt(args[0])));
+    }
+}
