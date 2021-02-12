e093eac1103492dd7ea04e290baec6f6dd821c07
DannyMeister177 <danny.goyer177@gmail.com>
2021-02-11 21:36:02 -0500

    Solved FizzBuzz (mine)

--- src/fizz_buzz/Solution.java
+++ src/fizz_buzz/Solution.java
@@ -1,17 +1,37 @@
 package fizz_buzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public static List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
         
-        return null;
+        for(int i=1; i<=n; i++){
+            boolean multipleOf3 = i%3==0;
+            boolean multipleOf5 = i%5==0;
+            if(multipleOf3 && multipleOf5) {
+                ans.add("FizzBuzz");
+                continue;
+            }
+            if(multipleOf3) { 
+                ans.add("Fizz");
+                continue;
+            }
+            if(multipleOf5) { 
+                ans.add("Buzz");
+                continue;
+            }
+            ans.add(Integer.toString(i));
+        }
+
+        return ans;
     }
 
     public static void main(String[] args){
         for (String string : args) {
-            System.out.println(string+" = "
-            + fizzBuzz(Integer.parseInt(string)));
+            System.out.println("\n\nn = "+string+"\n\n"
+            + fizzBuzz(Integer.parseInt(string))+"\n\n");
         }
     }
 }
