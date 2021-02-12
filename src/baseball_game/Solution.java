e48d9fb03a1fcd47c8186385fc565f1b04be9485
DannyMeister177 <danny.goyer177@gmail.com>
2021-02-12 18:15:20 -0500

    Solved Baseball Game

---
+++ src/baseball_game/Solution.java
@@ -0,0 +1,36 @@
+package baseball_game;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Solution {
+
+    public static int calPoints(String[] ops) {
+        Deque<Integer> stack = new ArrayDeque<>((int) (ops.length*0.90));
+
+        for (String s : ops) {
+            if(s.equals("C")) stack.pop();
+            else if(s.equals("D")) stack.push(2*stack.peek());
+            else if(s.equals("+")) {
+                int firstPrevScore = stack.pop();
+                int secondPrevScore = stack.peek();
+                stack.push(firstPrevScore);
+                stack.push(firstPrevScore+secondPrevScore);
+            }
+            else stack.push(Integer.parseInt(s));
+        }
+        int sum = 0;
+        for (int i : stack) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+        System.out.println("\n\nOperations are:\n"+Arrays.toString(args)+"\n\n");
+        System.out.println("Sum of ops is: "+calPoints(args));
+        System.out.println("\n\n");
+    }
+}
