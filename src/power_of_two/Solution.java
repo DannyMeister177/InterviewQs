a504675a8ec6665d20f34068d43878eb03954193
DannyMeister177 <danny.goyer177@gmail.com>
2021-02-11 20:16:18 -0500

    Solved Power of Two with bitwise

---
+++ src/power_of_two/Solution.java
@@ -0,0 +1,30 @@
+package power_of_two;
+
+class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        // first take care of a few edge cases for performance and to simplify code below
+        if (n == 1 || n == 1073741824) 
+            return true;
+        if ((n&1) == 1 || n <= 0) 
+            return false;
+
+        // now iterate through each bit position and count total number of one-bits
+        // if bitCount exceeds 1, n is not a power of two so return false
+        int mask = 2;
+        int bitCount = 0;
+        while (mask<=n && bitCount<=1){
+            if((mask&n)==mask) bitCount++;
+            if(mask>n) bitCount++;
+            mask <<= 1;
+        }
+
+        return bitCount==1;
+    }
+
+    public static void main(String[] args){
+        for (String string : args) {
+            System.out.println(string+" = "
+            + isPowerOfTwo(Integer.parseInt(string)));
+        }
+    }
+}
