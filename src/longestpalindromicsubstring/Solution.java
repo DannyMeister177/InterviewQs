3f76e5f394e88ebeda627eb226cfe8e11efe5792
DannyMeister177 <danny.goyer177@gmail.com>
2021-03-12 23:09:34 -0500

    Updating Longest plindromic substring

--- src/longestpalindromicsubstring/Solution.java
+++ src/longestpalindromicsubstring/Solution.java
@@ -1,12 +1,35 @@
 package src.longestpalindromicsubstring;
 
 public class Solution {
-    public String longestPalindrome(String s) {
-        return null;
+    public static String longestPalindrome(String s) {
+        int subStrSize = s.length();
+        while(subStrSize > 1){
+            for(int i=0; i+subStrSize<=s.length(); i++){
+                String subStr = s.substring(i, i+subStrSize);
+                if(isPalindrome(subStr)) return subStr;
+            }
+            subStrSize--;
+        }
+        // if code reaches here, no palindrome of siz > 1 so return one char
+        // since this is technically a palindrome of size 1
+        return s.substring(0, 1);
+    }
+
+    private static boolean isPalindrome(String s) {
+        int head = 0;
+        int tail = s.length() - 1;
+        while(head<tail){
+            if(s.charAt(head) != s.charAt(tail)) return false;
+            else {head++; tail--;}
+        }
+        // if code reaches here, all chars equal their counterpart so s is a palindrome
+        return true;
     }
 
     public static void main(String[] args){
-        System.out.println("");
-        
+        for (String string : args) {
+            System.out.print("String: "+string+" has longest palindromic substring: ");
+            System.out.println(longestPalindrome(string));
+        }      
     }
 }
